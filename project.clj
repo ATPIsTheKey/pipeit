68ca661c3ea8a459974315078c0ccf53bc26d55f
ATPIsTheKey <roland.fmn@gmail.com>
2021-06-18 15:01:16 +0300

    added json library to dump ast as a json object

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.match "1.0.0"]
+                 [org.clojure/data.json "2.3.1"]
                  [org.blancas/kern "1.1.0"]]
   :repl-options {:init-ns pipeit.core})
