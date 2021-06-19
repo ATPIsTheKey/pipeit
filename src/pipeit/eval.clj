c74da0decda0111f7748460e688a9b4814f26cc1
ATPIsTheKey <roland.fmn@gmail.com>
2021-06-19 15:27:37 +0300

    Implemented lazy evaluation of basic pipeit expressions. Refactored match expression in eval-expr to a case expression by using argument destructuring in the respective eval functions.

--- src/pipeit/eval.clj
+++ src/pipeit/eval.clj
@@ -6,10 +6,10 @@
 (defrecord Frame [parent-frame env])
 
 (def root-frame
-  (->Frame nil {}))
+  (->Frame nil (hash-map)))
 
 (defn new-frame [frame]
-  (->Frame frame {}))
+  (->Frame frame (hash-map)))
 
 (defn update-env [frame name val]
   (assoc-in frame [:env name] val))
@@ -18,22 +18,86 @@
   (or ((:env frame) name)
       (lookup-env (:parent-frame frame) name)))
 
-(declare eval-let-expr
-         eval-seq-expr)
+(declare eval-expr)
 
-(defn eval [frame ast-node]
-  (match [ast-node]
-         [{:node-type   :let-expr,
-           :rec?        _,                                  ; todo
-           :assignments assignments,
-           :body-expr   body-expr}] (eval-let-expr frame assignments body-expr)))
+(defn eval-let-expr [frame {assignments :assignments, body-expr :body-expr}]
+  (delay (let [updated-frame (reduce (fn [frame assignment]
+                                       (update-env frame (:name assignment) (eval-expr frame (:expr assignment))))
+                                     (new-frame frame)
+                                     assignments)]
+           (force (eval-expr updated-frame body-expr)))))
 
-(defn eval-let-expr [frame assignments body-expr]
-  (delay (eval (reduce (fn [frame assignment]
-                         (update-env frame (:name assignment) (:expr assignment)))
-                       (new-frame frame)
-                       assignments)
-               body-expr)))
+(defn eval-seq-expr [frame {node-type :node-type, exprs :exprs}]
+  (delay (->> exprs
+              (map (partial eval-expr frame))
+              (apply (case node-type
+                       :tuple-expr vector
+                       :list-expr list
+                       :set-expr set)))))
 
-(defn eval-seq-expr [frame exprs]
-  (map (partial eval frame) exprs))
+(defn eval-if-expr [frame {pred-expr :pred-expr, conseq-expr :conseq-expr, alt-expr :alt-expr}]
+  (delay (if (force (eval-expr frame pred-expr))
+           (eval-expr frame conseq-expr)
+           (eval-expr frame alt-expr))))
+
+(defn eval-when-expr [frame {pred-expr :pred-expr, conseq-expr :conseq-expr}]
+  (delay (when (force (eval-expr frame pred-expr))
+           (eval-expr frame conseq-expr))))
+
+(defn eval-fun-expr [frame {params :params, body-expr :body-expr :as ast-node}]
+  (if (empty? params)
+    (eval-expr frame body-expr)                             ; a function without arguments is just a value
+    (fn [x]
+      (-> frame
+          (update-env (first params) x)
+          (eval-fun-expr (assoc ast-node :params (rest params)))
+          force))))
+
+(defn eval-application-expr [frame {fun-expr :fun-expr, arg-expr :arg-expr}]
+  (delay ((force (eval-expr frame fun-expr)) (force (eval-expr frame arg-expr)))))
+
+(defn eval-unary-expr [frame {op :op, expr :expr}]
+  (delay (let [f (case op
+                   "-" -
+                   "+" +
+                   "not" not
+                   )]
+           (f (force (eval-expr frame expr))))))
+
+(defn eval-binary-expr [frame {op :op, lhs-expr :lhs-expr, rhs-expr :rhs-expr}]
+  (let [f (case op
+            "or" #(or %1 %2)
+            "and" #(and %1 %2)
+            "==" =
+            "not=" not=
+            ">" >
+            "<" <
+            "<=" <=
+            ">=" >=
+            "*" *
+            "%" mod
+            "//" quot
+            "+" +
+            "-" -
+            "**" #(Math/pow %1 %2))]
+    (delay (f (force (eval-expr frame lhs-expr))
+              (force (eval-expr frame rhs-expr))))))
+
+(defn eval-expr [frame {node-type :node-type :as ast-node}]
+  (case node-type
+    :let-expr (eval-let-expr frame ast-node)
+    :if-expr (eval-if-expr frame ast-node)
+    :when-expr (eval-when-expr frame ast-node)
+    :fun-expr (eval-fun-expr frame ast-node)
+    :application-expr (eval-application-expr frame ast-node)
+    '(:tuple-expr :list-expr :set-expr) (eval-seq-expr frame ast-node)
+    :unary-expr (eval-unary-expr frame ast-node)
+    :binary-expr (eval-binary-expr frame ast-node)
+    :literal-expr (:value ast-node)
+    :identifier-expr (lookup-env frame (:value ast-node))))
+
+(defn eval-string [s]
+  (->> s
+       p/parse-string
+       (map (comp force (partial eval-expr root-frame)))
+       (run! println)))
