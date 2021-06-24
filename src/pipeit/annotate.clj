3b206ce8afc1eea6ee83b0381518348927c77b7e
ATPIsTheKey <roland.fmn@gmail.com>
2021-06-24 19:40:25 +0300

    - Basic implementation of the variable type annotation: currently ignores the order of assignments in def stmts and let exprs. To correctly implement the recursion points annotation, this will have to be fixed.

--- src/pipeit/annotate.clj
+++ src/pipeit/annotate.clj
@@ -1,69 +1,104 @@
 (ns pipeit.annotate
   (:use [pipeit.utils])
-  (:require [clojure.set :as set])
-  (:use [clojure.core.match]))
-
-(defn annotate-recursion-points [ast-node]
-  (match [ast-node]
-         [{}]))
-
-(defn assoc-variable-types [ast-node & {:keys [vars free-vars bound-vars] :or {vars       #{},
-                                                                               free-vars  #{},
-                                                                               bound-vars #{}}}]
-  (merge ast-node {:vars       vars,
-                   :free-vars  free-vars,
-                   :bound-vars bound-vars}))
+  (:require [clojure.set :as set]))
 
 (defn annotate-variable-types [ast-node]
   (case (:node-type ast-node)
-    :identifier-expr (assoc-variable-types ast-node
-                                           :vars (hash-set val))
+    :program (update ast-node :stmts (partial map annotate-variable-types))
 
-    (:tuple-expr :list-expr :set-expr) (update ast-node :exprs (partial map annotate-variable-types))
+    :def-stmt (update :assignments ast-node (partial map-vals annotate-variable-types)) ; todo: ignores order of assignments
+
+    :identifier-expr (as-> ast-node node
+                           (assoc node :vars #{(:value node)})
+                           (assoc node :free-vars (:vars node))
+                           (assoc node :bound-vars #{}))
+
+    (list :tuple-expr
+          :list-expr
+          :set-expr) (as-> ast-node node
+                           (update node :exprs (partial map annotate-variable-types))
+                           (assoc node :vars (apply set/union
+                                                    (->> node :exprs (map :vars))))
+                           (assoc node :free-vars (apply set/union
+                                                         (->> node :exprs (map :free-vars))))
+                           (assoc node :bound-vars (set/difference (:vars node)
+                                                                   (:free-vars node))))
 
     :let-expr (as-> ast-node node
                     (update node :body-expr annotate-variable-types)
-                    (update node :assignments (partial map-vals annotate-variable-types))
-                    (assoc-variable-types node
-                                          :vars (apply set/union
-                                                       (->> node
-                                                            :body-expr
-                                                            :vars)
-                                                       (->> node
-                                                            :assignments
-                                                            vals
-                                                            (map :vars)))) ; a list of sets
-                    (assoc-variable-types :free-vars (apply set/difference
-                                                            (:vars node)
-                                                            (->> node
-                                                                 :assignments
-                                                                 keys
-                                                                 set))))
+                    (update node :assignments (partial map-vals annotate-variable-types)) ; todo: ignores order of assignments
+                    ;; the set of vars is the union of all sets of vars of body-expr and init-exprs
+                    (assoc node :vars (apply set/union
+                                             (->> node :body-expr :vars)
+                                             (->> node :assignments vals (map :vars))))
+                    ;; the set of free vars is the difference of the union of all sets in the init exprs and the body expr with the
+                    ;; set of assignment names
+                    (assoc node :free-vars (set/difference (apply set/union
+                                                                  (->> node :body-expr :free-vars)
+                                                                  (->> node :assignments vals (map :free-vars))) ; a seq of sets
+                                                           (->> node :assignments keys set)))
+                    ;; the set of bound vars is the difference of the set of all vars and the set of free vars
+                    (assoc node :bound-vars (set/difference (:vars node)
+                                                            (:free-vars node))))
 
     :unary-expr (as-> ast-node node
                       (update node :expr annotate-variable-types)
-                      (assoc-variable-types node
-                                            (select-keys (:expr node) [:vars :free-vars :bound-vars])))
+                      (assoc node :vars (->> node :expr :vars))
+                      (assoc node :free-vars (->> node :expr :free-vars))
+                      (assoc node :bound-vars (->> node :expr :bound-vars)))
 
     :binary-expr (as-> ast-node node
                        (update node :lhs-expr annotate-variable-types)
                        (update node :rhs-expr annotate-variable-types)
-                       (assoc-variable-types node
-                                             :vars (set/union (get-in node [:lhs-expr :vars])
-                                                              (get-in node [:rhs-expr :vars]))
-                                             :free-vars (set/union (get-in node [:lhs-expr :free-vars])
-                                                                   (get-in node [:rhs-expr :free-vars]))
-                                             :bound-vars (set/union (get-in node [:lhs-expr :bound-vars])
-                                                                    (get-in node [:rhs-expr :bound-vars]))))
+                       (assoc node :vars (set/union (->> node :lhs-expr :vars)
+                                                    (->> node :rhs-expr :vars)))
+                       (assoc node :free-vars (set/union (->> node :lhs-expr :free-vars)
+                                                         (->> node :rhs-expr :free-vars)))
+                       (assoc node :bound-vars (set/difference (:vars node)
+                                                               (:free-vars node))))
     :application-expr (as-> ast-node node
                             (update node :fun-expr annotate-variable-types)
                             (update node :arg-expr annotate-variable-types)
-                            (assoc-variable-types node
-                                                  :vars (set/union (get-in node [:lhs-expr :vars])
-                                                                   (get-in node [:rhs-expr :vars]))
-                                                  :free-vars (set/union (get-in node [:lhs-expr :free-vars])
-                                                                        (get-in node [:rhs-expr :free-vars]))
-                                                  :bound-vars (set/union (get-in node [:lhs-expr :bound-vars])
-                                                                         (get-in node [:rhs-expr :bound-vars]))))
-    (assoc-variable-types ast-node)))
+                            (assoc node :vars (set/union (->> node :fun-expr :vars)
+                                                         (->> node :arg-expr :vars)))
+                            (assoc node :free-vars (set/union (->> node :fun-expr :free-vars)
+                                                              (->> node :arg-expr :free-vars)))
+                            (assoc node :bound-vars (set/difference (:vars node)
+                                                                    (:free-vars node))))
+    :fun-expr (as-> ast-node node
+                    (update node :body-expr annotate-variable-types)
+                    (assoc node :vars (->> node :body-expr :vars))
+                    (assoc node :free-vars (set/difference (->> node :body-expr :free-vars)
+                                                           (->> node :params set)))
+                    (assoc node :bound-vars (set/difference (:vars node)
+                                                            (:free-vars node))))
 
+    :if-expr (as-> ast-node node
+                   (update node :pred-expr annotate-variable-types)
+                   (update node :conseq-expr annotate-variable-types)
+                   (update node :alt-expr annotate-variable-types)
+                   (assoc node :vars (set/union (->> node :pred-expr :vars)
+                                                (->> node :conseq-expr :vars)
+                                                (->> node :alt-expr :vars)))
+                   (assoc node :free-vars (set/union (->> node :pred-expr :free-vars)
+                                                     (->> node :conseq-expr :free-vars)
+                                                     (->> node :alt-expr :free-vars)))
+                   (assoc node :bound-vars (set/union (->> node :pred-expr :bound-vars)
+                                                      (->> node :conseq-expr :bound-vars)
+                                                      (->> node :alt-expr :bound-vars))))
+
+    :when-expr (as-> ast-node node
+                     (update node :pred-expr annotate-variable-types)
+                     (update node :conseq-expr annotate-variable-types)
+                     (assoc node :vars (set/union (->> node :pred-expr :vars)
+                                                  (->> node :conseq-expr :vars)))
+                     (assoc node :free-vars (set/union (->> node :pred-expr :free-vars)
+                                                       (->> node :conseq-expr :free-vars)))
+                     (assoc node :bound-vars (set/union (->> node :pred-expr :bound-vars)
+                                                        (->> node :conseq-expr :bound-vars))))
+
+    ;; default case
+    (as-> ast-node node
+          (assoc node :vars #{})
+          (assoc node :free-vars #{})
+          (assoc node :bound-vars #{}))))
