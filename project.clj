355db842a6c6a7c05a8e5b2c1c0b93fd7dac87d7
ATPIsTheKey <roland.fmn@gmail.com>
2021-06-24 17:37:35 +0300

    Added org.clojure/tools.trace as a dependency for easier debugging.

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.match "1.0.0"]
                  [org.clojure/data.json "2.3.1"]
+                 [org.clojure/tools.trace "0.7.11"]
                  [org.blancas/kern "1.1.0"]]
   :repl-options {:init-ns pipeit.core})
