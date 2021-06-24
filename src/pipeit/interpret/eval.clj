f4e2bfc46b8e07d6387865cc844bb7a480331625
ATPIsTheKey <roland.fmn@gmail.com>
2021-06-24 17:27:58 +0300

    - Implemented the evaluation of def statements. - Replaced eval-string with eval-prog which executes a sequence of statements that have an effect on the root frame. - Fixed a bug in eval eval-if-expr and eval-when-expr: once forced to evaluate, the respective branches must be forced evaluated, too.

--- src/pipeit/interpret/eval.clj
+++ src/pipeit/interpret/eval.clj
@@ -1,9 +1,10 @@
-(ns pipeit.eval
+(ns pipeit.interpret.eval
   (:require [pipeit.parse :as p])
   (:use [pipeit.utils])
-  (:require [clojure.core.match :refer [match]]))
+  (:require [clojure.core.match :refer [match]])
+  (:use [clojure.tools.trace]))
 
-(defrecord Frame [parent-frame env])
+(defrecord Frame [parent env])
 
 (def root-frame
   (->Frame nil (hash-map)))
@@ -16,15 +17,16 @@
 
 (defn lookup-env [frame name]
   (or ((:env frame) name)
-      (lookup-env (:parent-frame frame) name)))
+      (when-let [parent-frame (:parent frame)]
+        (lookup-env parent-frame name))))
 
 (declare eval-expr)
 
 (defn eval-let-expr [frame {assignments :assignments, body-expr :body-expr}]
-  (delay (let [updated-frame (reduce (fn [frame assignment]
-                                       (update-env frame (:name assignment) (eval-expr frame (:expr assignment))))
-                                     (new-frame frame)
-                                     assignments)]
+  (delay (let [updated-frame (reduce-kv (fn [frame init-name init-expr]
+                                          (update-env frame init-name (eval-expr frame init-expr)))
+                                        (new-frame frame)
+                                        assignments)]
            (force (eval-expr updated-frame body-expr)))))
 
 (defn eval-seq-expr [frame {node-type :node-type, exprs :exprs}]
@@ -33,16 +35,16 @@
               (apply (case node-type
                        :tuple-expr vector
                        :list-expr list
-                       :set-expr set)))))
+                       :set-expr hash-set)))))
 
 (defn eval-if-expr [frame {pred-expr :pred-expr, conseq-expr :conseq-expr, alt-expr :alt-expr}]
   (delay (if (force (eval-expr frame pred-expr))
-           (eval-expr frame conseq-expr)
-           (eval-expr frame alt-expr))))
+           (force (eval-expr frame conseq-expr))
+           (force (eval-expr frame alt-expr)))))
 
 (defn eval-when-expr [frame {pred-expr :pred-expr, conseq-expr :conseq-expr}]
   (delay (when (force (eval-expr frame pred-expr))
-           (eval-expr frame conseq-expr))))
+           (force (eval-expr frame conseq-expr)))))
 
 (defn eval-fun-expr [frame {params :params, body-expr :body-expr :as ast-node}]
   (if (empty? params)
@@ -54,14 +56,13 @@
           force))))
 
 (defn eval-application-expr [frame {fun-expr :fun-expr, arg-expr :arg-expr}]
-  (delay ((force (eval-expr frame fun-expr)) (force (eval-expr frame arg-expr)))))
+  (delay ((force (eval-expr frame fun-expr)) (eval-expr frame arg-expr)))) ; do not force evaluation of argument as it may not be used in the function body
 
 (defn eval-unary-expr [frame {op :op, expr :expr}]
   (delay (let [f (case op
                    "-" -
                    "+" +
-                   "not" not
-                   )]
+                   "not" not)]
            (f (force (eval-expr frame expr))))))
 
 (defn eval-binary-expr [frame {op :op, lhs-expr :lhs-expr, rhs-expr :rhs-expr}]
@@ -84,20 +85,39 @@
               (force (eval-expr frame rhs-expr))))))
 
 (defn eval-expr [frame {node-type :node-type :as ast-node}]
+  "Evaluates a pipeit expression. An expression does have no effects on the top frame it is evaluated with.
+  (frame stmt) -> object"
   (case node-type
     :let-expr (eval-let-expr frame ast-node)
     :if-expr (eval-if-expr frame ast-node)
     :when-expr (eval-when-expr frame ast-node)
     :fun-expr (eval-fun-expr frame ast-node)
     :application-expr (eval-application-expr frame ast-node)
-    '(:tuple-expr :list-expr :set-expr) (eval-seq-expr frame ast-node)
+    (:tuple-expr :list-expr :set-expr) (eval-seq-expr frame ast-node)
     :unary-expr (eval-unary-expr frame ast-node)
     :binary-expr (eval-binary-expr frame ast-node)
     :literal-expr (:value ast-node)
-    :identifier-expr (lookup-env frame (:value ast-node))))
+    :identifier-expr (force (lookup-env frame (:value ast-node)))))
 
-(defn eval-string [s]
+(defn eval-def-stmt [frame {assignments :assignments}]
+  (let [updated-frame (reduce-kv (fn [frame init-name init-expr]
+                                   (update-env frame init-name (eval-expr frame init-expr)))
+                                 frame
+                                 assignments)]
+    [updated-frame nil]))
+
+(defn eval-stmt [frame {node-type :node-type :as ast-node}]
+  "Evaluates a pipeit statement. A statement does have effects on the top frame it is evaluated with.
+  (frame stmt) -> (frame ?object)"
+  (case node-type
+    :def-stmt (eval-def-stmt frame ast-node)
+    [frame (eval-expr frame ast-node)]))                    ; evaluating an expression has no effects on the frame
+
+(defn eval-prog [s]
   (->> s
        p/parse-string
-       (map (comp force (partial eval-expr root-frame)))
-       (run! println)))
+       (reductions (fn [[frame _] stmt]
+                     (eval-stmt frame stmt))
+                   [root-frame nil])
+       (map (fn [[_ obj]] (force obj)))
+       rest))
